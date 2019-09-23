4879065fadde46b778ed8eec6c479977f8d406e3
luoyuan <luoyuan@fangdd.com>
2019-09-23 23:21:46 +0800

    完善用例
    Change-Id: I32dc82941dd96b81653c23f4307ea88943e586fa

--- src/testProject/test_shop/Estate_test.py
+++ src/testProject/test_shop/Estate_test.py
@@ -107,9 +107,140 @@ class EstateTest(RunTest):
             json_dict = self.a.json_data[self.project]["robot_data"]
             robot_url = json_dict["robot_url"]
             mobile = json_dict["mobile"]
-            send_ding(robot_url, mobile, content=f"我的楼盘列表异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            send_ding(robot_url, mobile, content=f"编辑楼盘基本信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
             raise err
 
+    @ddt.data(*a.get_data_by_api(fieldname, "PropertyEdit"))
+    def test_PropertyEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘物业信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
+
+    @ddt.data(*a.get_data_by_api(fieldname, "LayoutEdit"))
+    def test_LayoutEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘户型信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
+
+    @ddt.data(*a.get_data_by_api(fieldname, "BuildEdit"))
+    def test_BuildEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘楼栋信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
+
+    @ddt.data(*a.get_data_by_api(fieldname, "HouseHoldEdit"))
+    def test_HouseHoldEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘房源信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
+
+    @ddt.data(*a.get_data_by_api(fieldname, "DeveloperEdit"))
+    def test_DeveloperEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘开发商信息异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
+
+    @ddt.data(*a.get_data_by_api(fieldname, "SalesOffEdit"))
+    def test_SalesOffEdit(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        # 调用接口发起请求
+        res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+        try:
+            self.assertEqual(True, checkOut(self.res, self.expect))
+            self.logger.info("测试结果         :测试通过！")
+        except Exception as err:
+            self.logger.error("测试结果         :测试失败！")
+            json_dict = self.a.json_data[self.project]["robot_data"]
+            robot_url = json_dict["robot_url"]
+            mobile = json_dict["mobile"]
+            send_ding(robot_url, mobile, content=f"编辑楼盘销售概况异常，接口返回为：{res}, 接口预期结果为：{self.expect}")
+            raise err
 
 if __name__ == '__main__':
     unittest.main()
