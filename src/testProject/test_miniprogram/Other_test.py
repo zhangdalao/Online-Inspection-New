0a0fe88fb22cb1279f29e1ef7c4648c7dde772dc
luoyuan <luoyuan@fangdd.com>
2019-09-29 19:09:14 +0800

    [OPT]新房小程序测试用例
    Change-Id: I98c99ca82dd04ff4c0e1e9b3402b8e5ac5b6ed20

--- src/testProject/test_miniprogram/Other_test.py
+++ src/testProject/test_miniprogram/Other_test.py
@@ -25,6 +25,22 @@ class OtherTest(RunTest):
     # 通过类名获取模块名
     fieldname = sys._getframe().f_code.co_name[:-4]
 
+    @classmethod
+    def setUpClass(cls):
+        cls.env_num = cls.a.get_num_name("环境")
+        cls.apiName_num = cls.a.get_num_name("接口名称")
+        cls.method_num = cls.a.get_num_name("请求方法")
+        cls.headers_num = cls.a.get_num_name("请求头")
+        cls.para_num = cls.a.get_num_name("请求参数")
+        cls.desc_num = cls.a.get_num_name("用例描述")
+        cls.data_num = cls.a.get_num_name("请求体")
+        cls.expect_num = cls.a.get_num_name("预期结果")
+        cls.isSkip_num = cls.a.get_num_name("是否跳过该用例")
+        cls.relateData_num = cls.a.get_num_name("接口关联参数")
+        t = time.time()
+        cls.timestamp = str(round(t * 1000))
+        sss["timestamp"] = cls.timestamp
+
     def setUp(self):
         globals()['count'] += 1
         self.logger.debug("...start %s case %s...".center(80, '#') % (self.fieldname, count))
@@ -39,10 +55,14 @@ class OtherTest(RunTest):
         # 获取测试环境参数
         env = value[self.env_num]
         # 通过环境参数获得接口url
-        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = self.a.get_domains()[env] + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
         # 调用接口发起请求
         res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
-                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False, timeout=10)
         try:
             self.assertEqual(True, checkOut(self.res, self.expect))
             self.logger.info("测试结果         :测试通过！")
@@ -61,10 +81,14 @@ class OtherTest(RunTest):
         # 获取测试环境参数
         env = value[self.env_num]
         # 通过环境参数获得接口url
-        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = self.a.get_domains()[env] + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
         # 调用接口发起请求
         res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
-                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+                         self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False, timeout=10)
         try:
             self.assertEqual(True, checkOut(self.res, self.expect))
             self.logger.info("测试结果         :测试通过！")
@@ -83,7 +107,11 @@ class OtherTest(RunTest):
         # 获取测试环境参数
         env = value[self.env_num]
         # 通过环境参数获得接口url
-        url = self.a.get_domains()[env] + self.a.get_apiPath(self.fieldname, self.apiName)
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = self.a.get_domains()[env] + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
         # 调用接口发起请求
         res = self.start(self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
                          self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
