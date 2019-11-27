3c9b340ca487071edfeec2d43db04f507adeae55
chenwenjian <chenwenjian@fangdd.com>
2019-11-27 15:11:03 +0800

    调试demo3
    Change-Id: I33f3bbc2bef2b8eb72d410135eef435f86b524e8

--- src/mainProgram/run_server.py
+++ src/mainProgram/run_server.py
@@ -1,22 +1,37 @@
 from flask import Flask, request, jsonify
 
 from src.mainProgram.run import start
-from src.mainProgram.run_demo import start as s_demo
+from src.common.readConfData import GetDataIni
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+	return "Hello, World!"
 
 
 @app.route("/run_test")
 def run_test():
-    cases = request.args.get('cases', None)
-    env = request.args.get('env', None)
-    start.delay(cases_dir=cases, env=env)
-    return jsonify({"code": 200, "msg": "自动化测试已启动,请耐心等待钉钉推送执行结果！"})
+	try:
+		cases = request.args.get('cases', None)
+		env = request.args.get('env', None)
+		if cases == "All":
+			cases = None
+		start.delay(cases_dir=cases, env=env)
+		dataIni = GetDataIni()
+		p_name = dataIni.normal_data("Name", cases)
+		e_name = dataIni.normal_data("Env", env)
+	except KeyError:
+		res = jsonify({"code": 10001, "success": False, "msg": "自动化测试执行失败！请求参数不存在"})
+	except Exception:
+		res = jsonify({"code": 10002, "success": False, "msg": "自动化测试执行失败! 出现未知错误！"})
+	except ZeroDivisionError:
+		res = jsonify({"code": 10011, "success": True, "msg": "自动化测试结束！根据指定参数未获取到用例！"})
+	else:
+		res = jsonify({"code": 200, "success": True, "msg": f"{p_name}项目{e_name}环境自动化测试已启动,请耐心等待钉钉推送执行结果！"})
+	return res
 
 
 # @app.route("/start_test", methods=["post"])
@@ -27,5 +42,5 @@ def run_test():
 #     return jsonify(a)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1322)
+	app.run(host='0.0.0.0', port=1322)
 
