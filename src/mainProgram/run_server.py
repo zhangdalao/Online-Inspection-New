091a4e8ee36315025d66a99f8ee908033326ae55
chenwenjian <chenwenjian@fangdd.com>
2019-11-27 11:04:32 +0800

    调试demo
    Change-Id: Ib0fbf2e39e54b280916632745e981f428ff1b779

--- src/mainProgram/run_server.py
+++ src/mainProgram/run_server.py
@@ -11,20 +11,29 @@ def hello():
     return "Hello, World!"
 
 
+# @app.route("/run_test")
+# def run_test():
+#     cases = request.args.get('cases',None)
+#     env = request.args.get('env',None)
+#     start.delay(cases_dir=cases, env=env)
+#     return "自动化测试已启动"
+
+
 @app.route("/run_test")
 def run_test():
-    cases = request.args.get('cases',None)
-    env = request.args.get('env',None)
-    start.delay(cases_dir=cases, env=env)
-    return "自动化测试已启动"
+    cases = request.args.get('cases', None)
+    env = request.args.get('env', None)
+    a = start.delay(cases_dir=cases, env=env)
+    if a["code"] == 200:
+        return jsonify({"code": 200, "msg": "自动化测试已成功执行结束！"})
 
 
-@app.route("/start_test", methods=["post"])
-def start_test():
-    cases = request.form.get("cases")
-    env = request.form.get("env")
-    a = s_demo.delay(cases_dir=cases, env=env)
-    return jsonify(a)
+# @app.route("/start_test", methods=["post"])
+# def start_test():
+#     cases = request.form.get("cases")
+#     env = request.form.get("env")
+#     a = s_demo.delay(cases_dir=cases, env=env)
+#     return jsonify(a)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1322)
