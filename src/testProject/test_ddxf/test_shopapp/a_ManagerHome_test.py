71c088cf4106f69a9bcb79fd36838c58cb6efc55
luoyuan <luoyuan@fangdd.com>
2020-01-17 17:00:36 +0800

    新增SOP用例
    Change-Id: I3dbecdc9cd73ca934d07fcb3881e02dbf2e8d7ff

--- src/testProject/test_ddxf/test_shopapp/a_ManagerHome_test.py
+++ src/testProject/test_ddxf/test_shopapp/a_ManagerHome_test.py
@@ -10,6 +10,9 @@ import ddt
 import sys
 from src.common.runTest import *
 from src.common.dingDing import send_ding
+import datetime
+from datetime import timedelta
+import random
 
 count = 0
 
@@ -40,6 +43,27 @@ class ManagerHomeTest(RunTest):
         t = time.time()
         cls.timestamp = str(round(t * 1000))
         sss["timestamp"] = cls.timestamp
+        guidetime = datetime.datetime.now() + timedelta(hours=1)
+        cls.GuideTime = guidetime.strftime("%Y-%m-%d %H:%M:%S")
+        phone_num = random.randint(00000000, 99999999)
+        cls.phone_num = '199' + str(phone_num)
+        # 前三后四
+        hidephone = random.randint(0000, 9999)
+
+        # 前九后二
+        hidephone1 = random.randint(000000000, 999999999)
+
+        # 前五后二
+        hidephone2 = random.randint(00000, 99999)
+        cls.hidephone = '199' + '****' + str(hidephone)
+        cls.hidephone1 = str(hidephone1) + '**'
+        cls.hidephone2 = str(hidephone2) + '****' + '99'
+        sss["timestamp"] = cls.timestamp
+        sss["GuideTime"] = cls.GuideTime
+        sss["phonenum"] = cls.phone_num
+        sss["hidephone"] = cls.hidephone
+        sss["hidephone1"] = cls.hidephone1
+        sss["hidephone2"] = cls.hidephone2
 
     def setUp(self):
         globals()['count'] += 1
@@ -97,6 +121,23 @@ class ManagerHomeTest(RunTest):
 
     @ddt.data(*a.get_data_by_api(fieldname, "AuditData"))
     def test_AuditData(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[5:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = self.a.get_domains()[env] + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
+        # 调用接口发起请求
+        self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num,
+                                 self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
+                                 value, verify=False, timeout=10)
+
+    @ddt.data(*a.get_data_by_api(fieldname, "SopMon"))
+    def test_SopMon(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[5:]
         # 获取测试环境参数
@@ -214,8 +255,42 @@ class ManagerHomeTest(RunTest):
                                  self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
                                  value, verify=False, timeout=10)
 
+    @ddt.data(*a.get_data_by_api(fieldname, "Referral"))
+    def test1_Referral(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[6:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = str(sss["mfhost"]) + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["mfuserId"]), sss["mftoken"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
+        # 调起请求
+        self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                            self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+
+    @ddt.data(*a.get_data_by_api(fieldname, "SearchReferral"))
+    def test2_SearchReferral(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[6:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)
+        url = self.a.get_domains()[env] + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
+        # 调用接口发起请求
+        self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num,
+                                 self.headers_num,
+                                 self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
+                                 value, verify=False, timeout=10)
+
     @ddt.data(*a.get_data_by_api(fieldname, "ReferralList"))
-    def test1_ReferralList(self, value):
+    def test3_ReferralList(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[6:]
         # 获取测试环境参数
@@ -233,7 +308,7 @@ class ManagerHomeTest(RunTest):
         sss["referralIds"] = self.res['data']['pageData'][0]['referralId']
 
     @ddt.data(*a.get_data_by_api(fieldname, "ReferralConfirm"))
-    def test2_ReferralConfirm(self, value):
+    def test4_ReferralConfirm(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[6:]
         # 获取测试环境参数
@@ -249,8 +324,40 @@ class ManagerHomeTest(RunTest):
                                  self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
                                  value, verify=False, timeout=10)
 
+    @ddt.data(*a.get_data_by_api(fieldname, "Guide"))
+    def test5_Guide(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[6:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        uri = self.a.get_apiPath(self.fieldname, self.apiName) +str(sss["triangleId"])
+        url = str(sss["mfhost"]) + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["mfuserId"]), sss["mftoken"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
+        # 调起请求
+        self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                            self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+
+    @ddt.data(*a.get_data_by_api(fieldname, "Upcert"))
+    def test6_Upcert(self, value):
+        # 通过函数名获取apiName参数的值
+        self.apiName = (inspect.stack()[0][3])[6:]
+        # 获取测试环境参数
+        env = value[self.env_num]
+        # 通过环境参数获得接口url
+        uri = self.a.get_apiPath(self.fieldname, self.apiName) + str(sss["guideId"])
+        url = str(sss["mfhost"]) + uri
+        # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+        str_sign_list = [str(sss["mfuserId"]), sss["mftoken"], self.timestamp, value[self.method_num].upper(), uri]
+        value.append(str_sign_list)
+        # 调起请求
+        self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num, self.para_num,
+                            self.data_num, self.desc_num, self.relateData_num, self.expect_num, value, verify=False)
+
     @ddt.data(*a.get_data_by_api(fieldname, "CustomerDetail"))
-    def test3_CustomerDetail(self, value):
+    def test7_CustomerDetail(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[6:]
         # 获取测试环境参数
@@ -267,7 +374,7 @@ class ManagerHomeTest(RunTest):
                                  value, verify=False, timeout=10)
 
     @ddt.data(*a.get_data_by_api(fieldname, "GuideList"))
-    def test1_GuideList(self, value):
+    def test8_GuideList(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[6:]
         # 获取测试环境参数
@@ -282,12 +389,12 @@ class ManagerHomeTest(RunTest):
         self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num,
                                  self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
                                  value, verify=False, timeout=10)
-        sss["guideId"] = self.res['data']['pageData'][0]['guideId']
+        sss["guideId1"] = self.res['data']['pageData'][0]['guideId']
 
     @ddt.data(*a.get_data_by_api(fieldname, "GuideDetail"))
-    def test2_GuideDetail(self, value):
+    def test_GuideDetail(self, value):
         # 通过函数名获取apiName参数的值
-        self.apiName = (inspect.stack()[0][3])[6:]
+        self.apiName = (inspect.stack()[0][3])[5:]
         # 获取测试环境参数
         env = value[self.env_num]
         # 通过环境参数获得接口url
@@ -301,14 +408,32 @@ class ManagerHomeTest(RunTest):
                                  self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
                                  value, verify=False, timeout=10)
 
+    # @ddt.data(*a.get_data_by_api(fieldname, "GuideConfirm"))
+    # def test10_GuideConfirm(self, value):
+    #     # 通过函数名获取apiName参数的值
+    #     self.apiName = (inspect.stack()[0][3])[6:]
+    #     # 获取测试环境参数
+    #     env = value[self.env_num]
+    #     # 通过环境参数获得接口url
+    #     uri = self.a.get_apiPath(self.fieldname, self.apiName)+str(sss["guideId"])+"/confirm"
+    #     print("uri")
+    #     url = self.a.get_domains()[env] + uri
+    #     # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
+    #     str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
+    #     value.append(str_sign_list)
+    #     # 调用接口发起请求
+    #     self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num,
+    #                              self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
+    #                              value, verify=False, timeout=10)
+
     @ddt.data(*a.get_data_by_api(fieldname, "GuideConfirm"))
-    def test3_GuideConfirm(self, value):
+    def test9_GuideConfirm(self, value):
         # 通过函数名获取apiName参数的值
         self.apiName = (inspect.stack()[0][3])[6:]
         # 获取测试环境参数
         env = value[self.env_num]
         # 通过环境参数获得接口url
-        uri = self.a.get_apiPath(self.fieldname, self.apiName)+str(sss["guideId"])+"/confirm"
+        uri = self.a.get_apiPath(self.fieldname, self.apiName)+str(sss["guideId1"])+"/confirm"
         url = self.a.get_domains()[env] + uri
         # ***需要加密的数据在此处添加到列表中即可，反之则不用写这一步***
         str_sign_list = [str(sss["userId"]), sss["token"], self.timestamp, value[self.method_num].upper(), uri]
@@ -317,7 +442,3 @@ class ManagerHomeTest(RunTest):
         self.result = self.start(self.project, self.isSkip_num, self.apiName_num, url, self.method_num, self.headers_num,
                                  self.para_num, self.data_num, self.desc_num, self.relateData_num, self.expect_num,
                                  value, verify=False, timeout=10)
-
-
-if __name__ == '__main__':
-    unittest.main()
